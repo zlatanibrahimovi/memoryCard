0544348f81b72feb59535be855d2e0786187430f
zlatanibrahimovi <131520250+zlatanibrahimovi@users.noreply.github.com>
2023-04-22 13:38:53 +0500

    Add files via upload

---
+++ my_memory_card.py
@@ -0,0 +1,103 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QRadioButton,  
+        QPushButton, QLabel, QButtonGroup)
+ 
+app = QApplication([])
+ 
+# Создаем панель вопроса
+btn_OK = QPushButton('Ответить')
+lb_Question = QLabel('Самый сложный вопрос в мире!')
+ 
+RadioGroupBox = QGroupBox("Варианты ответов")
+ 
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+
+RadioGroup = QButtonGroup() 
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_4)
+ 
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+ 
+RadioGroupBox.setLayout(layout_ans1)
+ 
+# Создаем панель результата
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
+lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
+ 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+ 
+# Размещаем все виджеты в окне:
+layout_line1 = QHBoxLayout() # вопрос
+layout_line2 = QHBoxLayout() # варианты ответов или результат теста
+layout_line3 = QHBoxLayout() # кнопка "Ответить"
+ 
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+# Размещаем в одной строке обе панели, одна из них будет скрываться, другая показываться:
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+ 
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+ 
+# Теперь созданные строки разместим друг под другой:
+layout_card = QVBoxLayout()
+ 
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+AnsGroupBox.hide()
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+
+
+def show_question():
+    AnsGroupBox.hide()
+    RadioGroupBox.show()
+    btn_OK.setText('Ответить')
+    RadioGroup.setExclusive(False) # сняли ограничения, чтобы можно было сбросить выбор радиокнопки
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True) # вернули ограничения, теперь только одна радиокнопка может быть выбрана
+
+
+def start_test():
+    if btn_OK.text() == 'Ответить':
+        show_result()
+    else:
+        show_question()
+
+ 
+window = QWidget()
+window.setLayout(layout_card)
+window.setWindowTitle('Memory Card')
+btn_OK.clicked.connect(start_test)
+window.show()
+app.exec()
